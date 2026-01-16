e1c7455c9876e08ba7ae29721ccbe014a1becfa3
Coty Janeway <coty.janeway@gmail.com>
2026-01-15 20:27:23 -0700

    basci frontend with vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [, set] = useState(true);
+  return (
+    <>
+      <div>
+        Hi
+      </div>
+    </>
+  )
+}
+
+export default App
