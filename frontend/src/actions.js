197e4edfd73a44869c83256b787b295cfb3f9e4c
Coty Janeway <coty.janeway@gmail.com>
2026-01-19 14:47:44 -0700

    casing fix

--- frontend/src/actions.js
+++ frontend/src/actions.js
@@ -4,13 +4,13 @@ const ACTIONS = {
     {startFrame: 10, button: "forward", duration: 1},
     {startFrame: 11, button: "back", duration: 1},
     {startFrame: 12, button: "forward", duration: 1},
-    {startFrame: 12, button: "X", duration: 2},
+    {startFrame: 12, button: "x", duration: 2},
   ],
   hadoken: [
     {startFrame: 0, button: "down", duration: 1},
     {startFrame: 1, button: "down-forward", duration: 1},
     {startFrame: 2, button: "forward", duration: 1},
-    {startFrame: 3, button: "X", duration: 1},
+    {startFrame: 3, button: "x", duration: 1},
   ],
 }
 
