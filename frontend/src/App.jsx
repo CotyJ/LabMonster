931350ba597d15e54c9216df7802fa50427d7174
Coty Janeway <coty.janeway@gmail.com>
2026-01-19 13:30:31 -0700

    normalize again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,32 @@
-import { useEffect, useState } from 'react'
 import axios from 'axios';
 import './App.css';
 
 const host = "http://localhost:8000"
 const baseRoute = "/push-button";
 
-const fakeData = {
-  actions: [
-    {startFrame: 1, button: "back", holdFor: 30},
-    {startFrame: 32, button: "forward", holdFor: 1},
-    {startFrame: 33, button: "back", holdFor: 1},
-    {startFrame: 34, button: "forward", holdFor: 1},
-    {startFrame: 35, button: "X", holdFor: 1},
-]}
+import ACTIONS from './actions';
 
 function App() {
-  // const [data, setData] = useState({});
   const pushButton = (dataJSON) => {
     axios.post(`${host}${baseRoute}`, dataJSON)
       .then( res => {
         console.log(res.data);
-        // setData(res.data);
       })
       .catch( err => console.log(err.message));
   }
 
-  // useEffect(() => {}, [data])
-
-
   return (
-    <>
-      <div>
-        <button onClick={() => pushButton(fakeData)}>Send</button>
+    <div className='container'>
+      <div className="buttons">
+
+        {Object.entries(ACTIONS).map(([name, inputs]) => (
+          <div key={name} className='row m-2'>
+            <button onClick={() => pushButton(inputs)}>{name}</button>
+          </div>
+        ))}
+
       </div>
-    </>
+    </div>
   )
 }
 
---
+++ frontend/src/actions.js
@@ -0,0 +1,17 @@
+const ACTIONS = {
+  charge_super: [
+    {startFrame: 1, button: "back", duration: 9},
+    {startFrame: 10, button: "forward", duration: 1},
+    {startFrame: 11, button: "back", duration: 1},
+    {startFrame: 12, button: "forward", duration: 1},
+    {startFrame: 12, button: "X", duration: 2},
+  ],
+  hadoken: [
+    {startFrame: 0, button: "down", duration: 1},
+    {startFrame: 1, button: "down-forward", duration: 1},
+    {startFrame: 2, button: "forward", duration: 1},
+    {startFrame: 3, button: "X", duration: 1},
+  ],
+}
+
+export default ACTIONS;
