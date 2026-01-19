1d168a3945291be69d0811f3379cc68a70509abf
Coty Janeway <coty.janeway@gmail.com>
2026-01-19 00:19:12 -0700

    connect front end to back end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,37 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+import './App.css';
+
+const host = "http://localhost:8000"
+const baseRoute = "/push-button";
+
+const fakeData = {
+  actions: [
+    {startFrame: 1, button: "back", holdFor: 30},
+    {startFrame: 32, button: "forward", holdFor: 1},
+    {startFrame: 33, button: "back", holdFor: 1},
+    {startFrame: 34, button: "forward", holdFor: 1},
+    {startFrame: 35, button: "X", holdFor: 1},
+]}
 
 function App() {
+  // const [data, setData] = useState({});
+  const pushButton = (dataJSON) => {
+    axios.post(`${host}${baseRoute}`, dataJSON)
+      .then( res => {
+        console.log(res.data);
+        // setData(res.data);
+      })
+      .catch( err => console.log(err.message));
+  }
+
+  // useEffect(() => {}, [data])
+
 
   return (
     <>
       <div>
-        Hi
+        <button onClick={() => pushButton(fakeData)}>Send</button>
       </div>
     </>
   )
