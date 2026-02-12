e8028ca29bc56c6d1534dd3902e1dfcc3b1a2e1d
Coty Janeway <coty.janeway@gmail.com>
2026-02-12 15:55:53 -0700

    remove iteration

--- backend/executor.py
+++ backend/executor.py
@@ -1,11 +1,18 @@
 # a class for executing the instructions sent from API
-import time
+# import time
 class Executor:
   def __init__(self, timeline):
     self.timeline = timeline
+    # Create controller
 
+  # Run sequence
   def run(self):
-    for i, frame in enumerate(self.timeline):
-      print(i, frame)
-      frame = 1 / 60
-      time.sleep(frame)
+    print("Starting sequence...")
+    # for i, frame in enumerate(self.timeline):
+    #   print(i, frame)
+    #   frame = 1 / 60
+    #   time.sleep(frame)
+
+  # Destroy controller
+  def tini():
+    print("Goodbye")
